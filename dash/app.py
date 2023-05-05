730916f0f5a8843cc6aaec99f26c47fd92b001ac
Arshitha <arsh2794@gmail.com>
2023-05-05 16:54:39 -0400

    basic reactive shiny app

---
+++ dash/app.py
@@ -0,0 +1,83 @@
+
+from shiny import App, render, ui, reactive
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import statsmodels.formula.api as smf
+from io import StringIO
+from pathlib import Path
+
+sns.set_theme()
+
+data = pd.read_csv('/Users/arshithab/Desktop/OneDesk/Projects/MEGDash/MEGHack_dashboard/data/TESTDATA.csv')
+
+options_group = data["group"].unique().tolist()
+options_parcel = data["Parcel"].unique().tolist()
+
+app_ui = ui.page_fluid(
+    ui.panel_title("MEG Dashboard"),
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.input_selectize("group", "Group", options_group, multiple=False),
+            ui.input_selectize("parcel", "Parcel", options_parcel, multiple=False),
+            # ui.input_slider("min_age", "Minimum Age", 0, 114, 20),
+            # ui.input_slider("max_age", "Maximum Age", 0, 114, 80),
+        ),
+        ui.panel_main(
+            ui.output_text("proc_parcel_regression"),
+            ui.output_plot("a_scatter_plot"),
+            ui.output_table("show_table"),
+        ),
+    ),
+)
+
+
+def server(input, output, session):
+    @reactive.Calc
+    def subset_df():
+        # print(input.group())
+        indx_group = data["group"].isin([int(i) for i in input.group()])
+        indx_parcel = data["Parcel"].isin([input.parcel()])
+
+        # subset data to keep only selected group
+        sub_df = data[indx_group & indx_parcel]
+        # sub_df = sub_df[sub_df['age'] > input.min_age()]
+        # sub_df = sub_df[sub_df['age'] < input.max_age()]
+        # print(proc_parcel_regression())
+        return sub_df
+
+    @reactive.Calc
+    def group_age_range():
+        age_range = []
+        age_range[0] = data["group"].isin([int(input.group())]).age.min()
+        age_range[1] = data["group"].isin([int(input.group())]).age.min()
+        return age_range
+
+    @reactive.Calc
+    def group_subj_count():
+        subj_ct = data.groupby(['group']).get_group(int(input.group())).nunique().subject
+        return subj_ct
+
+    @reactive.Calc
+    def proc_parcel_regression():
+        '''Takes merged dataframe (subj_vars + parcel data)
+        and performs regression of variables using statsmodels'''
+        df = subset_df()
+        results = smf.ols('Delta ~ age', data=df).fit()
+
+        return results.summary()
+
+    @output
+    @render.table
+    def show_table():
+        df = subset_df()
+        return df
+
+    @output
+    @render.plot(alt="Scatter Plot: Age vs Delta")
+    def a_scatter_plot():
+        sub_df = subset_df()
+        return sns.regplot(data=sub_df, x='age', y='Alpha')
+
+
+app = App(app_ui, server)
